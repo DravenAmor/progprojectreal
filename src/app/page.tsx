19087b6938dc4377fbb7a7a7daa53035684026da
[Draven Amor] <[2121300489@k12.hi.us]>
2024-10-23 09:04:10 -1000

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import Link from "next/link";
+import { db } from "~/server/db"
 
+
+/*
 const mockUrls = [
   "https://utfs.io/f/06079463-903c-4d25-a12e-1172fbdbd99d-1ody.jpg", 
   "https://utfs.io/f/5accb5ae-66b7-4f8d-9ba7-445f7aa08ac1-2a.jpg",
@@ -12,14 +15,18 @@ const mockImages = mockUrls.map((url, index) =>({
   id: index + 1,
   url,
 }));
+*/
 
 export default function HomePage() {
+  const images = await db.query.images.findMany();
   return (
     <main className="">
-      <div className="">
-        {mockImages.map((image) => (
+      <div className= "flex flex-wrap gap-4">
+        {images.map((image) => (
           <div key={image.id} className="w-48">
             <img src={image.url} />
+            <div>{image.name} </div>
+            <img className ="object-none object-center bg-yellow w-24 h-24 ..." src={image.url} />
       </div>
       ))}
       </div>
